48a7c340e62226f87abf142fe95cd40f62944db0
Mark Romero <markromero@marks-mbp.lan>
2021-02-16 20:06:37 -0700

    inital setup done

---
+++ foundation/web/web.go
@@ -0,0 +1,9 @@
+// Package web contains a small web framework extension.
+package web
+
+import "github.com/dimfeld/httptreemux/v5"
+
+type App struct {
+	//this makes it so app is everything a context mux - this is calleld embedding
+	httptreemux.ContextMux
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mromero1591/service
+
+go 1.16
+
+require (
+	github.com/ardanlabs/conf v1.3.3
+	github.com/dimfeld/httptreemux/v5 v5.2.2
+	github.com/pkg/errors v0.9.1
+)
